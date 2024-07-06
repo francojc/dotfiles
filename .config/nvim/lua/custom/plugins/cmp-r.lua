87ba11463191bc8acfe6731b58188b71b8edb19b
Jerid Francom <francojc@wfu.edu>
2024-07-06 01:11:43 -0400

    Misc updates

--- .config/nvim/lua/custom/plugins/cmp-r.lua
+++ .config/nvim/lua/custom/plugins/cmp-r.lua
@@ -5,7 +5,7 @@ return {
 
     cmpr.setup {
       filetypes = { 'r', 'rmd', 'quarto', 'rhelp' },
-      doc_width = 58,
+      doc_width = 80,
       quarto_intel = '/Applications/quarto/share/editor/tools/yaml/yaml-intelligence-resources.json',
     }
   end,
