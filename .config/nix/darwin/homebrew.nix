7544d06ee18e6696e2740206afa6088081ed0a30
Jerid Francom <francojc@wfu.edu>
2024-08-14 22:58:07 -0400

    updates nix home packages

--- .config/nix/darwin/homebrew.nix
+++ .config/nix/darwin/homebrew.nix
@@ -7,37 +7,36 @@
       cleanup = "zap";
       upgrade = true;
     };
-    taps = [ ];
+    taps = [
+      "nikitabobko/tap" # for aerospace
+      "FelixKratz/formulae" # for jankyborders
+    ];
     brews = [
       "llm" # permission to install models
+      "borders" # jankyborders
     ];
     casks = [
+      "aerospace" # have not tested this (possible replacement for yabai/skhd)
       "appcleaner"
-      "arc"
-      "audacity"
+      "balenaetcher"
       "bettermouse"
-      "bitwarden"
+      "bitwarden" # (only on nix linux)
       "calibre"
       "chromium"
-      "datasette"
       "docker"
-      "drawio"
       "dropbox"
       "espanso"
-      "font-hack"
-      "font-hack-nerd-font"
-      "font-meslo-lg-nerd-font"
-      "font-symbols-only-nerd-font"
+      "fantastical"
       "google-drive"
       "jordanbaird-ice"
       "kap"
       "mullvadvpn"
-      "obsidian"
+      "obsidian" # (only on nix linux)
       "praat"
       "raycast"
-      "rectangle"
       "sequel-ace"
       "transcribe"
+      "transmission"
       "zotero@beta"
     ];
     masApps = { };
