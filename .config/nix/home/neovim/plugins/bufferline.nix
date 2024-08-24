916910e05b0c96f51d50f06df950c83f7745af73
francojc <francojc@wfu.edu>
2024-08-24 08:37:35 -0400

    modifies the tab look, simpler

--- .config/nix/home/neovim/plugins/bufferline.nix
+++ .config/nix/home/neovim/plugins/bufferline.nix
@@ -3,7 +3,12 @@
     plugins.bufferline = {
       enable = true;
       settings = {
-        options.separator_style = "thin";
+        options = {
+          separator_style = "thin"; # "thin" | "thick" | "slant" | "round" | "default"
+          modified_icon = "✎"; # pencil
+          color_icons = false;
+          show_buffer_icons = false;
+        };
       };
     };
   };
