654d9e26faac929750759956eaeffcd535d159e5
francojc <francojc@wfu.edu>
2024-08-24 10:01:58 -0400

    adding flake to the ~/.bin

---
+++ .bin/README.md
@@ -0,0 +1,40 @@
+# Personal scripts and tools
+
+This repository contains a collection of scripts and tools that I have created to automate tasks, improve productivity, or just for fun. The scripts are written in Python, Bash, and other languages.
+
+<!--
+TODO: add a flake to replace the venv/ requirements and renv environment(s)
+
+-->
+
+```sh
+~/.bin
+├── README.md
+├── am
+├── am-art.applescript
+├── battery_status.sh
+├── bin.code-workspace
+├── fetch-student-merge
+├── fetch-student-remote
+├── git-all-acp
+├── google-drive-files.sh
+├── imagey
+├── macchange
+├── macfind
+├── network_info
+├── preview_watch
+├── push-pull-request
+├── render_watch
+├── search_mutt.sh
+├── tmp.jpg
+├── to_nvim.sh
+├── transcribe
+├── venv
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   └── pyvenv.cfg
+├── weather
+└── wifi_status.zsh
+
+```
---
+++ .bin/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Nix flake for R/Python/shell development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # Base packages
+        basePackages = with pkgs; [
+          bashInteractive
+          gettext
+          gh
+          git
+          pandoc
+          python312
+          quarto
+          R
+          radianWrapper
+        ];
+
+        # Python packages
+        pythonPackages = with pkgs.python312; [
+          argparse
+          os
+          openai
+          requests
+        ];
+
+        # R packages
+        rPackages = with pkgs.rPackages; [
+          # Utils
+          devtools
+          knitr
+          languageserver
+          rmarkdown
+          # Project
+          tidyverse
+        ];
+
+        allPackages = basePackages ++ rPackages ++ pythonPackages;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = allPackages;
+          shellHook = ''
+            export R_LIBS_USER=$PWD/R/Library; mkdir -p $R_LIBS_USER;
+          '';
+        };
+      });
+}
