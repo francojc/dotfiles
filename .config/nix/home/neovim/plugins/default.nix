d5788a226d34796b105b327b0c0c2e418d632165
francojc <francojc@wfu.edu>
2024-11-05 19:59:54 -0500

    changes nvim to gruvbox theme

--- .config/nix/home/neovim/plugins/default.nix
+++ .config/nix/home/neovim/plugins/default.nix
@@ -23,24 +23,25 @@
 
       base16 = {
         enable = true;
-        colorscheme = {
-          base00 = "#000000";
-          base01 = "#121212";
-          base02 = "#222222";
-          base03 = "#888888";
-          base04 = "#999999";
-          base05 = "#c1c1c1";
-          base06 = "#999999";
-          base07 = "#c1c1c1";
-          base08 = "#5f8787";
-          base09 = "#aaaaaa";
-          base0A = "#a06666";
-          base0B = "#dd9999";
-          base0C = "#aaaaaa";
-          base0D = "#888888";
-          base0E = "#999999";
-          base0F = "#b58900";
-        };
+        colorscheme = "gruvbox-dark-hard";
+        # {
+        # base00 = "#000000";
+        # base01 = "#121212";
+        # base02 = "#222222";
+        # base03 = "#888888";
+        # base04 = "#999999";
+        # base05 = "#c1c1c1";
+        # base06 = "#999999";
+        # base07 = "#c1c1c1";
+        # base08 = "#5f8787";
+        # base09 = "#aaaaaa";
+        # base0A = "#a06666";
+        # base0B = "#dd9999";
+        # base0C = "#aaaaaa";
+        # base0D = "#888888";
+        # base0E = "#999999";
+        # base0F = "#b58900";
+        # };
       };
       gruvbox = {
         enable = false;
