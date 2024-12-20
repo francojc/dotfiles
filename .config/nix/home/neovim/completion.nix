4e5271823f828fffe5e1b4faecce4e3134a94943
francojc <francojc@wfu.edu>
2024-12-20 02:02:18 -0500

    refactor: Simplify and update Neovim completion configuration

--- .config/nix/home/neovim/completion.nix
+++ .config/nix/home/neovim/completion.nix
@@ -10,122 +10,59 @@
         enable = true;
         filetypeExtend = {
           quarto = [ "markdown" ];
+          rmarkdown = [ "markdown" ];
         };
         settings = {
           enable_autosnippets = true;
         };
         fromVscode = [
           { }
-          { paths = "./snippets"; }
+          { paths = [ (builtins.path { path = ./snippets; }) ]; }
         ];
       };
 
-      blink-cmp = {
+      cmp = {
         enable = true;
+        autoEnableSources = true;
+        filetype = {
+          markdown = {
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "luasnip"; }
+              { name = "path"; }
+            ];
+          };
+        };
         settings = {
-          accept = {
-            auto_brackets = {
-              enabled = true;
-              semantic_token_resolution.enabled = true;
-            };
-          };
-          trigger = {
-            signature_help.enabled = true;
-          };
-          keymap = {
-            "<C-u>" = [
-              "scroll_documentation_up"
-              "fallback"
-            ];
-            "<C-e>" = [
-              "hide"
-            ];
-            "<C-d>" = [
-              "scroll_documentation_down"
-              "fallback"
-            ];
-            "<Tab>" = [
-              "select_next"
-              "fallback"
-            ];
-            "<S-Tab>" = [
-              "select_prev"
-              "fallback"
-            ];
-            "<C-space>" = [
-              "show"
-              "show_documentation"
-              "hide_documentation"
-            ];
-            "<C-y>" = [
-              "select_and_accept"
-            ];
-            "<Left>" = [
-              "snippet_backward"
-              "fallback"
-            ];
-            "<Right>" = [
-              "snippet_forward"
-              "fallback"
-            ];
-
-          };
           completion = {
-            documentation = {
-              auto_show = true;
-              auto_show_delay_ms = 500;
-            };
-            list = {
-              selection = "manual";
-            };
-            menu = {
-              draw = {
-                gap = 2;
-                treesitter = true;
-                columns = [
-                  {
-                    __unkeyed-1 = "label";
-                    __unkeyed-2 = "label_description";
-                    gap = 1;
-                  }
-                  {
-                    __unkeyed-1 = "kind_icon";
-                    __unkeyed-2 = "kind";
-                    gap = 1;
-                  }
-                ];
-                components = {
-                  label = {
-                    width.fill = true;
-                  };
-                  "kind_icon" = {
-                    width.fill = true;
-                  };
-                };
-              };
-            };
+            completeopt = "menu,menuone,noselect";
           };
-          sources = {
-            completion.enabled_providers = [
-              "lsp"
-              "path"
-              "luasnip"
-              "snippets"
-              "buffer"
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+          snippet = {
+            expand =
+              ''
+                function(args)
+                  require('luasnip').expand(args.body)
+                end
+              '';
+          };
+          sources =
+            [
+              { name = "nvim_lsp_signature_help"; }
+              { name = "nvim_lsp"; }
+              { name = "luasnip"; }
+              { name = "pandoc_references"; }
+              { name = "path"; }
+              { name = "buffer"; keyword_length = 5; }
             ];
-            providers = {
-              snippets = {
-                opts = {
-                  extended_filetypes = {
-                    quarto = [ "markdown" ];
-                  };
-                };
-              };
-            };
-          };
-          signature = {
-            enabled = true;
-          };
         };
       };
     };
