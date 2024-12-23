dd02e350f209af32dc8bc0d59ff93eebd95c18c5
francojc <francojc@wfu.edu>
2024-12-23 18:25:47 -0500

    update yazi alias zsh and nushell

--- .config/nix/home/shell/default.nix
+++ .config/nix/home/shell/default.nix
@@ -18,45 +18,55 @@
     nushell = {
       enable = true;
       extraConfig = ''
-         # -- Configuration for Nu shell --
-         $env.config = {
-           show_banner: false,
-           edit_mode: vi,
-         }
+          # -- Configuration for Nu shell --
+          $env.config.show_banner = false
+          $env.config.edit_mode = "vi"
+          $env.config.rm.always_trash = false
 
-         # -- Configure path for Nu shell --
-         $env.PATH = ($env.PATH | split row ":"
-           | prepend "/usr/local/sbin"
-           | prepend "/nix/var/nix/profiles/default/bin"
-           | prepend "/run/current-system/sw/bin"
-           | prepend "/etc/profiles/per-user/francojc/bin"
-           | prepend "/Users/francojc/.nix-profile/bin"
-           | prepend "/opt/homebrew/sbin"
-           | prepend "/opt/homebrew/bin"
-           | prepend "/Users/francojc/.bin"
-           | prepend "/Users/francojc/.local/bin"
-           | uniq
-           )
+          # -- Configure path for Nu shell --
+          $env.PATH = ($env.PATH | split row ":"
+            | prepend "/usr/local/sbin"
+            | prepend "/nix/var/nix/profiles/default/bin"
+            | prepend "/run/current-system/sw/bin"
+            | prepend "/etc/profiles/per-user/francojc/bin"
+            | prepend "/Users/francojc/.nix-profile/bin"
+            | prepend "/opt/homebrew/sbin"
+            | prepend "/opt/homebrew/bin"
+            | prepend "/Users/francojc/.bin"
+            | prepend "/Users/francojc/.local/bin"
+            | uniq
+            )
 
-         $env.PROMPT_INDICATOR_VI_NORMAL = '❮ ';
+          $env.PROMPT_INDICATOR_VI_NORMAL = '❮ ';
 
-         $env.EDITOR = 'nvim'
-         $env.VISUAL = 'nvim'
-         $env.HOMEBREW_NO_ENV_HINTS = 'true'
+          $env.EDITOR = 'nvim'
+          $env.VISUAL = 'nvim'
+          $env.HOMEBREW_NO_ENV_HINTS = 'true'
 
-        open ~/.variables.env
-        | lines
-        | each { |line|
-            let parts = ($line | str replace "export " "" | split row "=" | take 2)
-            let key = ($parts.0 | str trim)
-            let value = ($parts.1 | str trim)
-            {$key: $value}
+          open ~/.variables.env
+          | lines
+          | each { |line|
+              let parts = ($line | str replace "export " "" | split row "=" | take 2)
+              let key = ($parts.0 | str trim)
+              let value = ($parts.1 | str trim)
+              {$key: $value}
+          }
+          | reduce -f {} { |it, acc| $acc | merge $it }
+          | load-env
+
+          alias .. = cd ..
+          alias ... = cd ../..
+
+
+          def --env y [...args] {
+            let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+            yazi ...$args --cwd-file $tmp
+            let cwd = (open $tmp)
+            if $cwd != "" and $cwd != $env.PWD {
+              cd $cwd
+            }
+          rm -fp $tmp
         }
-        | reduce -f {} { |it, acc| $acc | merge $it }
-        | load-env
-
-         alias .. = cd ..
-         alias ... = cd ../..
       '';
       shellAliases = {
         # -- Shell aliases for Nu shell --
