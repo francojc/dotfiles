b9537a16677086f9ac6eb5badb5f32cee3a7154d
francojc <francojc@wfu.edu>
2025-02-02 19:18:33 -0500

    aider: fix: Update SearXNG secret key with a generated value

--- .config/nix/modules/nix-core.nix
+++ .config/nix/modules/nix-core.nix
@@ -24,7 +24,7 @@
         general = {
           debug = false;
           instance_name = "My SearXNG";
-          secret_key = "@REPLACE_ME_WITH_A_SECRET_KEY@";  # Generate with: openssl rand -hex 32
+          secret_key = "7a1937f3e3a6205382ec7427e3439e0533b7902962c5769fe641148a6aebcf25";  # Generate with: openssl rand -hex 32
         };
       };
     };
