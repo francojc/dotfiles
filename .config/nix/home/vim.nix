2d24376972fed4f4f929bc17ba5db7efecdead00
francojc <francojc@wfu.edu>
2025-02-18 13:47:06 -0500

    formatting .vimrc

--- .config/nix/home/vim.nix
+++ .config/nix/home/vim.nix
@@ -10,6 +10,7 @@
         mini-files # File explorer
       ];
       extraConfig = ''
+        " GENERAL --------------------------------------------------
         set number              " Show line numbers
         set relativenumber      " Show relative line numbers
         set tabstop=4           " Tab width
@@ -23,6 +24,8 @@
 
         syntax on               " Syntax highlighting
         set mouse=a             " Enable mouse support
+
+        " KEYMAPS --------------------------------------------------
         " Switch between splits
         map <C-j> <C-W>j
         map <C-k> <C-W>k
@@ -34,8 +37,10 @@
 
         colorscheme quiet       " Simple colorscheme
 
+        " AUTOCOMMANDS ---------------------------------------------
         " Highlight on Yank
         au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=200}
+
       '';
     };
   };
