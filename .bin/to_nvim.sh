8380c3487c106d7cd0401520c0fc11c5f2b290df
francojc <francojc@wfu.edu>
2025-03-02 17:01:02 -0500

    chore: Remove unused to_nvim.sh script

--- .bin/to_nvim.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script takes a list of files and opens them in neovim in a new WezTerm terminal.
-
-for f in "$@"
-do
-    open -na "WezTerm" --args -e nvim "$f"
-done
-
