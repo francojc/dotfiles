e5555748b96cdaf9bae0aa8359180c949d1e1ce3
francojc <francojc@wfu.edu>
2025-03-30 18:33:30 -0400

    chore: remove nushell configuration and dependencies

--- .config/nix/home/shell/default.nix
+++ .config/nix/home/shell/default.nix
@@ -52,80 +52,24 @@
         lg = "lazygit";
       };
     };
-    nushell = {
-      enable = false;
-      extraConfig = ''
-          # -- Configuration for Nu shell --
-          $env.config.show_banner = false
-          $env.config.edit_mode = "vi"
-          $env.config.rm.always_trash = false
-
-          # -- Configure path for Nu shell --
-          $env.PATH = ($env.PATH | split row ":"
-            | prepend "/usr/local/sbin"
-            | prepend "/nix/var/nix/profiles/default/bin"
-            | prepend "/run/current-system/sw/bin"
-            | prepend "/etc/profiles/per-user/${username}/bin"
-            | prepend "/Users/${username}/.nix-profile/bin"
-            | prepend "/opt/homebrew/sbin"
-            | prepend "/opt/homebrew/bin"
-            | prepend "/Users/${username}/.bin"
-            | prepend "/Users/${username}/.local/bin" | uniq)
-
-          $env.PROMPT_INDICATOR_VI_NORMAL = '❮ ';
-
-          $env.EDITOR = 'nvim'
-          $env.VISUAL = 'nvim'
-          $env.HOMEBREW_NO_ENV_HINTS = 'true'
-
-          open ~/.variables.env
-          | lines
-          | each { |line|
-              let parts = ($line | str replace "export " "" | split row "=" | take 2)
-              let key = ($parts.0 | str trim)
-              let value = ($parts.1 | str trim)
-              {$key: $value}
-          }
-          | reduce -f {} { |it, acc| $acc | merge $it }
-          | load-env
-
-          alias .. = cd ..
-          alias ... = cd ../..
-
-
-          def --env y [...args] {
-            let tmp = (mktemp -t "yazi-cwd.XXXXXX")
-            yazi ...$args --cwd-file $tmp
-            let cwd = (open $tmp)
-            if $cwd != "" and $cwd != $env.PWD {
-              cd $cwd
-            }
-          rm -fp $tmp
-        }
-      '';
-    };
 
     # Enable some useful tools
     atuin = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
     };
     carapace = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
     };
     direnv = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
       nix-direnv.enable = true;
     };
     eza = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
     };
     fzf = {
       enable = true;
@@ -134,12 +78,10 @@
     starship = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
     };
     zoxide = {
       enable = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
     };
   };
 }
