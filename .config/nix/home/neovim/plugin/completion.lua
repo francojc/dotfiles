e2aa487fbc4ea012a5fe5d4c97d864028b1eeed2
francojc <francojc@wfu.edu>
2025-04-02 12:05:24 -0400

    chore: update completion plugin configuration

--- .config/nix/home/neovim/plugin/completion.lua
+++ .config/nix/home/neovim/plugin/completion.lua
@@ -9,22 +9,22 @@ require("blink.cmp").setup({
 	signature = { enabled = true },
 	snippets = {
 		preset = "luasnip",
-		-- expand = function(snippet)
-		-- 	vim.snippet.lsp_expand(snippet)
-		-- end,
-		-- active = function(filter)
-		-- 	return vim.snippet.active(filter)
-		-- end,
-		-- jump = function(direction)
-		-- 	return vim.snippet.jump(direction)
-		-- end,
+		expand = function(snippet)
+			vim.snippet.lsp_expand(snippet)
+		end,
+		active = function(filter)
+			return vim.snippet.active(filter)
+		end,
+		jump = function(direction)
+			return vim.snippet.jump(direction)
+		end,
 	},
 	sources = {
 		default = { "path", "snippets", "lsp" },
-		providers = {
-			snippets = {
-				opts = { search_paths = os.getenv("HOME") .. "/.config/nix/home/neovim/snippets" },
-			},
-		},
+		-- providers = {
+		-- 	snippets = {
+		-- 		opts = { search_paths = os.getenv("HOME") .. "/.config/nix/home/neovim/snippets" },
+		-- 	},
+		-- },
 	},
 })
