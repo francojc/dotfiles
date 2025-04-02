60d8fe74b9a28ae587f23617b88723bd7c5f28e9
francojc <francojc@wfu.edu>
2025-04-02 12:09:26 -0400

    feat: enable auto brackets in completion setup

--- .config/nix/home/neovim/plugin/completion.lua
+++ .config/nix/home/neovim/plugin/completion.lua
@@ -1,12 +1,10 @@
 require("blink.cmp").setup({
-	appearance = { use_nvim_cmp_as_default = true },
 	completion = {
 		accept = { auto_brackets = { enabled = true } },
 		menu = { auto_show = false },
 		documentation = { auto_show = true },
 	},
 	keymap = { preset = "enter" },
-	signature = { enabled = true },
 	snippets = {
 		preset = "luasnip",
 		expand = function(snippet)
