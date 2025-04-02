c870198751fcf65894552ee191ab4047be754070
francojc <francojc@wfu.edu>
2025-04-02 10:58:34 -0400

    chore: update neovim completion plugin configuration

--- .config/nix/home/neovim/plugin/completion.lua
+++ .config/nix/home/neovim/plugin/completion.lua
@@ -1,4 +1,10 @@
 require("blink.cmp").setup({
+	menu = {
+		auto_show = false,
+		documentation = {
+			auto_show = true,
+		},
+	},
 	appearance = {
 		use_nvim_cmp_as_default = true,
 	},
@@ -10,5 +16,7 @@ require("blink.cmp").setup({
 	},
 	sources = {
 		default = { "path", "snippets", "lsp" },
+		snippets = { preset = "luasnip" },
+		signature = { enabled = true },
 	},
 })
