6dba82f2fb4839e3fb0228e28b548d3b58cc0231
francojc <francojc@wfu.edu>
2025-04-02 11:09:14 -0400

    refactor: simplify completion.lua configuration

--- .config/nix/home/neovim/plugin/completion.lua
+++ .config/nix/home/neovim/plugin/completion.lua
@@ -1,22 +1,12 @@
 require("blink.cmp").setup({
-	appearance = {
-		use_nvim_cmp_as_default = true,
-	},
+	appearance = { use_nvim_cmp_as_default = true },
 	completion = {
 		accept = { auto_brackets = { enabled = true } },
-		menu = {
-			auto_show = false,
-		},
-		documentation = {
-			auto_show = true,
-		},
+		menu = { auto_show = false },
+		documentation = { auto_show = true },
 	},
-	keymap = {
-		preset = "enter",
-	},
-	sources = {
-		default = { "path", "snippets", "lsp" },
-		snippets = { preset = "luasnip" },
-		signature = { enabled = true },
-	},
+	keymap = { preset = "enter" },
+	signature = { enabled = true },
+	snippets = { preset = "luasnip" },
+	sources = { default = { "path", "snippets", "lsp" } },
 })
