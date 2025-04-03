ad0d64e31f965fae17af3253275a44da1259934e
francojc <francojc@wfu.edu>
2025-04-03 11:43:52 -0400

    chore: update kitty theme to Black Metal and update completion config

--- .config/nix/home/neovim/plugin/completion.lua
+++ .config/nix/home/neovim/plugin/completion.lua
@@ -1,9 +1,4 @@
 require("blink.cmp").setup({
-	completion = {
-		menu = { auto_show = true },
-		documentation = { auto_show = true, auto_show_delay_ms = 500 },
-	},
 	keymap = { preset = "enter" },
-	snippets = {},
 	sources = { default = { "path", "snippets", "lsp" } },
 })
