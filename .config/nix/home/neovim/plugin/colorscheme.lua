36c22a92cd9cea71e0901dc38d922eb60213287b
francojc <francojc@wfu.edu>
2025-04-06 10:03:23 -0400

    chore: switch to tokyonight colorscheme

--- .config/nix/home/neovim/plugin/colorscheme.lua
+++ .config/nix/home/neovim/plugin/colorscheme.lua
@@ -5,5 +5,10 @@ require("gruvbox").setup({
 	invert_selection = true,
 })
 
+-- Tokyonight
+require("tokyonight").setup({
+	style = "night",
+})
+
 -- Set colorscheme
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme tokyonight")
