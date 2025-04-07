16b3866095315114a513351286de18aa85144d99
francojc <francojc@wfu.edu>
2025-04-07 09:39:32 -0400

    chore: disable format on save for neovim lsp plugin

--- .config/nix/home/neovim/plugin/lsp.lua
+++ .config/nix/home/neovim/plugin/lsp.lua
@@ -118,10 +118,10 @@ require("conform").setup({
 		quarto = { "air", "prettier" },
 		["_"] = { "trim_whitespace" },
 	},
-	format_on_save = {
-		lsp_format = "fallback",
-		timeout_ms = 500,
-	},
+	-- format_on_save = {
+	-- 	lsp_format = "fallback",
+	-- 	timeout_ms = 500,
+	-- },
 })
 
 require("conform").formatters.mdformat = {
