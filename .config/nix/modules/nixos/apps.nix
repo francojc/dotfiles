6823f8262a524b5fcccf5122dd57ddab7a84233c
francojc <francojc@wfu.edu>
2025-05-03 23:28:30 -0400

    chore: Remove gnome prefix from gnome-tweaks package

--- .config/nix/modules/nixos/apps.nix
+++ .config/nix/modules/nixos/apps.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = with pkgs; [
     # Keep only NixOS-specific system packages here, if any.
     # Example: gnome packages if not pulled in by services.xserver.desktopManager.gnome.enable
-    gnome.gnome-tweaks # Example of a potentially NixOS-specific addition
+    gnome-tweaks # Example of a potentially NixOS-specific addition
     # Add any other system-level tools here
   ];
 }
