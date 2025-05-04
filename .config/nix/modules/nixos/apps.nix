262d206eb50d5ddd723564e3e0cefd0b916add6b
francojc <francojc@wfu.edu>
2025-05-04 17:21:27 -0400

    adding dconf2nix

--- .config/nix/modules/nixos/apps.nix
+++ .config/nix/modules/nixos/apps.nix
@@ -3,10 +3,13 @@
   environment.systemPackages = with pkgs; [
     # Keep only NixOS-specific system packages here, if any.
     # Example: gnome packages if not pulled in by services.xserver.desktopManager.gnome.enable
+    dconf2nix
     ghostty
     gnome-tweaks
     gnomeExtensions.dash-to-dock
     gnomeExtensions.clipboard-indicator
+    gnomeExtensions.extension-list
+    gnomeExtensions.awesome-tiles
     xclip
   ];
 }
