2befa749817e2344a53efdd7728ba65b4cae9804
francojc <francojc@wfu.edu>
2025-08-09 21:52:18 -0400

    feat: add autocommand to remove trailing whitespace on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -152,6 +152,17 @@ vim.diagnostic.config({
 -- Create an autocommand group
 -- personal group
 a.nvim_create_augroup("personal", { clear = true })
+-- Remove trailing whitespace on save
+a.nvim_create_autocmd("BufWritePre", {
+	group = "personal",
+	pattern = "*",
+	callback = function()
+		local pos = vim.api.nvim_win_get_cursor(0)
+		vim.cmd([[%s/\s\+$//e]])
+		vim.api.nvim_win_set_cursor(0, pos)
+	end,
+})
+
 -- Highlight on yank
 a.nvim_create_autocmd("TextYankPost", {
 	group = "personal",
