ce99017d05e1c18dfd57017ed2f3535f28c51224
francojc <francojc@wfu.edu>
2025-08-17 09:03:41 -0400

    feat: add sound notification for session idle event

--- .config/opencode/plugins/sound-on-idle.js
+++ .config/opencode/plugins/sound-on-idle.js
@@ -2,8 +2,8 @@ export const SoundOnIdlePlugin = async ({ $, client }) => {
   return {
     event: async ({ event }) => {
       if (event.type === "session.idle") {
+        // Play a sound when the session is idle
         await $`afplay /System/Library/Sounds/Glass.aiff`
-        await $`osascript -e 'display notification "Session completed!" with title "opencode"'`
       }
     }
   }
