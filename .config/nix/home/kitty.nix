22d6bbd2c4e787128da88506f7d2dabb699546c3
francojc <francojc@wfu.edu>
2026-02-10 09:41:02 -0500

    chore: increase kitty font sizes and add app icon

--- .config/nix/home/kitty.nix
+++ .config/nix/home/kitty.nix
@@ -23,10 +23,10 @@
 
       font_size ${toString (
         if hostname == "Mac-Minicore"
-        then 19
+        then 20
         else if hostname == "Macbook-Airborne"
-        then 15
-        else 13
+        then 16
+        else 14
       )}
 
       ## Advanced Font Rendering
