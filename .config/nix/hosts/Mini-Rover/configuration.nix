1068276278721f3105a42b97a013080f0b507e98
francojc <francojc@wfu.edu>
2026-03-30 11:57:44 -0400

    feat: disable sleep modes to ensure SSH availability

--- .config/nix/hosts/Mini-Rover/configuration.nix
+++ .config/nix/hosts/Mini-Rover/configuration.nix
@@ -11,6 +11,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # --- Prevent Sleep (keep available for SSH) ---
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   # --- SSH Server ---
   services.openssh = {
     enable = true;
